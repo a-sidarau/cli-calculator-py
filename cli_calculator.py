794e441dc862075ca074bfa0be429363089ad991
Anton Sidarau <anthony.sidarau@gmail.com>
2025-06-17 03:03:38 +0300

    Add contorls options

--- cli_calculator.py
+++ cli_calculator.py
@@ -13,46 +13,68 @@ def divide(a, b):
     else:
         return a / b
     
-def times(a, b):
+def exponentiate(a, b):
     return a ** b 
+
+def modulus(a, b):
+    if a or b == 0:
+        print("Cannot divide by 0!")
+    return a % b
 
 operations = {
     '+':add,
     '-':substract,
     '/':divide,
     '*':multiply,
-    '**':times
+    '**':exponentiate,
+    '%':modulus
 }
-\
-print("Starting simple Python calculator")
 
-while True:
+def controls():
+    print("\n-------------------")
+    print("Simple Terminal Calculator on Python")
+    print("\n-------------------")
+    print("Select operation:")
+    print("1. Add")
+    print("2. Substract")
+    print("3. Divide")
+    print("4. Multiply")
+    print("5. a²")
+    print("6. Modulus %")
+    print("9. Exit")
+    print("\n-------------------")
+
+
+def get_input():
     # Принимаем значение от пользователя и пытаемся привести к численному типу
     # Если введено не число, то приведение типа выдаст ValueError
     # Ловим исключение try-except блоком и начинаем цикл сначала
-    try:
-        num1 = float(input("\nEnter first number: "))
-    except ValueError:
-        print("This is not a number. Please enter a valid one\n")
-        continue
+    while True:
+        try:
+            num1 = float(input("\nEnter first number: "))
+        except ValueError:
+            print("This is not a number. Please enter a valid one\n")
+            continue
 
-    # Проверяем, что введённая операция (символ) есть в словаре
-    operation = input("\nEnter operation (+, -, /, *, **): ")
-    if operation not in operations:
-        print("\nInvalid operation type! Enter one of the valid ones: +, -, /, *, **")
-        continue
+        # Проверяем, что введённая операция (символ) есть в словаре
+        operation = input("\nEnter operation (+, -, /, *, **): ")
+        if operation not in operations:
+            print("\nInvalid operation type! Enter one of the valid ones: +, -, /, *, **")
+            continue
 
-    try:
-        num2 = float(input("\nEnter second number: "))
-    except ValueError:
-        print("This is not a number. Please enter a valid one\n")
-        continue
+        try:
+            num2 = float(input("\nEnter second number: "))
+        except ValueError:
+            print("This is not a number. Please enter a valid one\n")
+            continue
 
-    if operation in operations:
-        print(operations[operation](num1, num2))
-    
-    choice = input("\nDo you wish to continue? [y/n]\n")
-    if choice.lower() in ['y', 'yes']:
-        continue
-    else:
-        break
+def main():
+    print("Starting simple Python calculator")
+
+    while True:
+        if operation in operations:
+            print(f"{num1} {operation} {num2} = {operations[operation](num1, num2)}")
+
+
+if __name__ == "__main__":
+    main()
